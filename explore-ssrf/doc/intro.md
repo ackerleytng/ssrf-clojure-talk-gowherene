3b3cb5e3cb7276cc3cbdf894be822668507381b9
ackerleytng <ackerleytng@gmail.com>
2019-06-05 18:14:54 +0800

    Add clojure code to test SSRF

---
+++ explore-ssrf/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to explore-ssrf
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ explore-ssrf/test/explore_ssrf/core_test.clj
@@ -0,0 +1,7 @@
+(ns explore-ssrf.core-test
+  (:require [clojure.test :refer :all]
+            [explore-ssrf.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
